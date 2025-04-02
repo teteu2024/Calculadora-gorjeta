f872b82d5d26e0770bcd4004545731346582d98a
matheusmurija863 <matheusmurija863@gmail.com>
2025-04-02 00:12:53 +0000

    Recebendo o valor da porcentagem ao clicar no botao

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
+
+function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+}
+
+function receiveNumberOfPeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value){
+    tipPercentage = value / 100
+
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+    }
+
+    buttonSelected = document.querySelector('#button-${value}')
+    buttonSelected.classList.add("button-selected")
+}
